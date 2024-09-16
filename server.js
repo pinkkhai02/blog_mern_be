254642fa6f36568e3bda9ea0d77048315b72161d
Phạm Hồng Khải <pinkkhai02@gmail.com>
2024-09-16 17:07:16 +0700

    code: api-detail user profile

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import bcrypt from "bcrypt";
 import { nanoid } from "nanoid";
 import jwt from "jsonwebtoken";
+import cors from "cors";
+import admin from "firebase-admin";
+import serviceAccountKey from "./path/serviceAccountKey.json" assert { type: "json" };
+import { getAuth } from "firebase-admin/auth";
+import aws from "aws-sdk";
 
 //schema below
 import User from "./Schema/User.js";
+import Blog from "./Schema/Blog.js";
 
 const server = express();
 let PORT = 3000;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 server.use(express.json());
+server.use(cors());
 
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
@@ -44,11 +55,40 @@ const generateUsername = async (email) => {
   return username;
 };
 
+//setting up s3 bucket
+const s3 = new aws.S3({
+  region: "ap-southeast-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const generateUploadURL = async () => {
+  const date = new Date();
+  const imageName = `${nanoid()}-${date.getTime()}.jpeg`;
+
+  return await s3.getSignedUrlPromise("putObject", {
+    Bucket: "upload-image-2",
+    Key: `upload-image/${imageName}`,
+    Expires: 60 * 3,
+    ContentType: "image/jpeg",
+  });
+};
+
+//upload img url route
+server.get("/get-upload-url", (req, res) => {
+  generateUploadURL()
+    .then((url) => res.status(200).json({ uploadURL: url }))
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).json({ error: err.message });
+    });
+});
+
 server.post("/signup", (req, res) => {
   const { fullname, email, password } = req.body;
 
   // validating the data from frontend
-  if (fullname.length < 3) {
+  if (fullname !== undefined && fullname !== null && fullname.length < 3) {
     return res
       .status(403)
       .json({ error: "Fullname must be at least 3 characters" });
@@ -93,24 +133,318 @@ server.post("/signin", (req, res) => {
       if (!user) {
         return res.status(403).json({ error: "Email not found" });
       }
-      bcrypt.compare(password, user.personal_info.password, (err, result) => {
-        if (err) {
-          return res
-            .status(403)
-            .json({ error: "Error occured while login please try again" });
-        }
 
-        if (!result) {
-          return res.status(403).json({ error: "Incorrect password" });
-        } else {
-          return res.status(200).json(formatDatatoSend(user));
-        }
-      });
+      if (!user.google_auth) {
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+          if (err) {
+            return res
+              .status(403)
+              .json({ error: "Error occured while login please try again" });
+          }
+
+          if (!result) {
+            return res.status(403).json({ error: "Incorrect password" });
+          } else {
+            return res.status(200).json(formatDatatoSend(user));
+          }
+        });
+      } else {
+        return res.status(403).json({
+          error:
+            "Account was created using google. Try logging in with google.",
+        });
+      }
 
       // return res.status(403).json({ error: "got user document" });
     })
     .catch((err) => {
       console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/google-auth", async (req, res) => {
+  let { access_token } = req.body;
+  getAuth()
+    .verifyIdToken(access_token)
+    .then(async (decodeUser) => {
+      let { email, name, picture } = decodeUser;
+
+      picture = picture.replace("s96-c", "s384-c");
+
+      let user = await User.findOne({ "personal_info.email": email })
+        .select(
+          "personal_info.fullname personal_info.username personal_info.profile_img google_auth"
+        )
+        .then((u) => {
+          return u || null;
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+
+      if (user) {
+        //login
+        if (!user.google_auth) {
+          return res.status(403).json({
+            error:
+              "This email was signed up without google. Please log ign with access the account",
+          });
+        }
+      } else {
+        // sign up
+        let username = await generateUsername(email);
+
+        user = new User({
+          personal_info: {
+            fullname: name,
+            email,
+            profile_img: picture,
+            username,
+          },
+          google_auth: true,
+        });
+
+        await user
+          .save()
+          .then((u) => {
+            user = u;
+          })
+          .catch((err) => {
+            return res.status(500).json({ error: err.message });
+          });
+      }
+      return res.status(200).json(formatDatatoSend(user));
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        error:
+          "Failed to authenticate you with goole. Try with some other google account",
+      });
+    });
+});
+
+const verifiJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({ error: "No access token " });
+  }
+
+  jwt.verify(token, process.env.SECRET_ACCESS_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Access toke is invalid" });
+    }
+    req.user = user.id;
+    next();
+  });
+};
+
+server.post("/create-blog", verifiJWT, (req, res) => {
+  // console.log(req.body);
+  let authorId = req.user;
+
+  let { title, des, banner, tags, content, draft } = req.body;
+
+  if (!title.length) {
+    return res.status(403).json({ err: "You must provide a title  the blog" });
+  }
+  if (!draft) {
+    if (!des.length || des.length > 200) {
+      return res.status(403).json({
+        err: "You must provide blog description under 200 characters",
+      });
+    }
+    if (!banner.length) {
+      return res
+        .status(403)
+        .json({ err: "You must provide blog banner to publish the blog" });
+    }
+    if (!content.blocks.length) {
+      return res
+        .status(403)
+        .json({ err: "There must be some blog content to publish it" });
+    }
+    if (!tags.length || tags.length > 10) {
+      return res
+        .status(403)
+        .json({ err: "Provide tags in order to publish, maximum 10" });
+    }
+  }
+
+  tags = tags.map((tag) => tag.toLowerCase());
+
+  let blog_id =
+    title
+      .replace(/[^a-zA-Z0-9]/g, " ")
+      .replace(/\s+/g, "-")
+      .trim() + nanoid();
+
+  // console.log(blogId);
+
+  let blog = new Blog({
+    title,
+    des,
+    banner,
+    content,
+    tags,
+    author: authorId,
+    blog_id,
+    draft: Boolean(draft),
+  });
+
+  blog
+    .save()
+    .then((blog) => {
+      let incrementVal = draft ? 0 : 1;
+
+      User.findOneAndUpdate(
+        { _id: authorId },
+        {
+          $inc: { "account_info.total_posts": incrementVal },
+          $push: { blogs: blog._id },
+        }
+      )
+        .then((user) => {
+          return res.status(200).json({ id: blog.blog_id });
+        })
+        .catch((err) => {
+          return res
+            .status(500)
+            .json({ error: "Failed to update total posts numder" });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+
+  // return res.json({ stus: "done" });
+});
+
+server.post("/latest-blogs", (req, res) => {
+  let maxLimit = 2;
+  const { page } = req.body;
+
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/all-latest-blogs-count", (req, res) => {
+  Blog.countDocuments({ draft: false })
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.get("/trending-blogs", (req, res) => {
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({
+      "activity.total_reads": -1,
+      "activity.total_likes": -1,
+      publishedAt: -1,
+    })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-blogs", (req, res) => {
+  let maxLimit = 2;
+  let { tag, page, query } = req.body;
+  let findQuery;
+
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  }
+
+  Blog.find(findQuery)
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+server.post("/search-blogs-count", (req, res) => {
+  let findQuery;
+  let { tag, query } = req.body;
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  }
+  Blog.countDocuments(findQuery)
+    .then((count) => {
+      res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-users", (req, res) => {
+  let { query } = req.body;
+
+  User.find({ "personal_info.username": new RegExp(query, "i") })
+    .limit(50)
+    .select(
+      "personal_info.fullname personal_info.username personal_info.profile_img -_id"
+    )
+    .then((users) => {
+      return res.status(200).json({ users });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/get-profile", (req, res) => {
+  let { username } = req.body;
+
+  User.find({ "personal_info.username": username })
+    .select("-personal_info.password -google_auth -blogs -updateAt")
+    .then((user) => {
+      return res.status(200).json(user);
+    })
+    .catch((err) => {
       return res.status(500).json({ error: err.message });
     });
 });
